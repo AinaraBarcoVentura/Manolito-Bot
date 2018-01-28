ed1fc81bd9b9895aca43f3deabe66a42aeed759d
AinaraBarcoVentura <ainaradelbarcoventura@gmail.com>
2018-01-28 15:14:00 +0100

    Start command added

--- Manolito.py
+++ Manolito.py
@@ -6,8 +6,13 @@ def hello(bot, update):
         'Hello {}'.format(update.message.from_user.first_name))
 
 
+def start(bot, update):
+	update.message.reply_text(
+		'Hola, soy Manolito, tu bot favorito :)')
+
 updater = Updater('455286497:AAH7LZcEHYaB4X2eo_RoM7rqz7OGve1dB3I')
 
+updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('holamundo', hello))
 
 updater.start_polling()
